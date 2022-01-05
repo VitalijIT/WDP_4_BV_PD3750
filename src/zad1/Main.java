4af813c23240f42233d84b357ea6e4dfb86db192
VitalijIT <vitalibodnar@gmail.com>
2022-01-05 22:48:23 +0100

    zad1 - cleaned

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -15,26 +15,26 @@ public class Main {
 
     if (args.length < 2) syntax();
 
-    String str = args[0]; // "ala ma kota, ale  ma psa.psa.hopsa. psa kota psa ma ala";
-    String findStr = args[1]; // "sto";
+    String str = args[0];
+    String findStr = args[1];
     int strLength = str.length();
 
-    System.out.println(strLength); // n1
+    System.out.println(strLength);
 
-    System.out.println(str.charAt(0) + " " + str.charAt(strLength - 1)); // n2
+    System.out.println(str.charAt(0) + " " + str.charAt(strLength - 1));
 
-    System.out.println(str.substring(4)); // n3
+    System.out.println(str.substring(4));
 
-    System.out.println(str.substring(4, strLength - 1)); // n4
+    System.out.println(str.substring(4, strLength - 1));
 
     SubstringCounter sCounter = new SubstringCounter();
-    System.out.println(sCounter.countSubstrings(str, findStr)); // n5
+    System.out.println(sCounter.countSubstrings(str, findStr));
 
     StringTransformer strTransformer = new StringTransformer();
     String[] strArr = strTransformer.transformStringToArray(str);
-    System.out.println(String.join("_", strArr)); // n6
+    System.out.println(String.join("_", strArr));
 
-    System.out.println(strArr[0].equals(strArr[strArr.length - 1])); // n7
+    System.out.println(strArr[0].equals(strArr[strArr.length - 1]));
 
   }
 
